c1cf47508c765b15da57cbff26f962bdf07625b3
Michael Essex <112738703+messex1@users.noreply.github.com>
2024-04-27 15:33:56 -0600

    Done

--- js/main.js
+++ js/main.js
@@ -1,29 +1,66 @@
-/*
-Steps 1-3 READ THE PDF!
-*/
 (function () {
   let videoGameForm = document.querySelector('#video-game-form');
   let allGameList = document.querySelector('.all-games');
-  let allGameListItems = document.querySelectorAll('.all-games li');
   let myGameList = document.querySelector('.my-favourite-games');
   let myGames = [];
 
-  // event listener for step 1
+  // Event listener for form submission to filter games
   videoGameForm.addEventListener('submit', (event) => {
     event.preventDefault();
-    let platform = event.target.elements['platform-family'].value.toLowerCase();
+    let platform = event.target.elements['platform-family'].value;
+    filterGames(platform);
   });
 
-  // event listener for step 2
-  /* HTML for step 2 to add to the list
-  <li class="list-group-item">VIDEO GAME NAME HERE</li>
-  */
+  function filterGames(platform) {
+    const allGameListItems = document.querySelectorAll('.all-games li');
+    allGameListItems.forEach(item => {
+      if (item.innerText.toLowerCase().includes(platform.toLowerCase())) {
+        item.classList.remove('hidden-item');
+      } else {
+        item.classList.add('hidden-item');
+      }
+    });
+  }
+
+
+  // Event listener for adding games to the favourites list
   allGameList.addEventListener('click', (event) => {
-    let game = event.target.innerText;
+    if (event.target && event.target.nodeName === 'LI') {
+      let game = event.target.innerText;
+      addToFavouriteGames(game);
+    }
   });
 
-  // event listener for step 3
+  // Add a game to the favourites array and update the UI
+  function addToFavouriteGames(game) {
+    if (!myGames.includes(game)) {
+      myGames.push(game);
+      renderFavouriteList();
+    }
+  }
+
+  // Render the favourites list
+  function renderFavouriteList() {
+    myGameList.innerHTML = '';
+    myGames.forEach(game => {
+      myGameList.innerHTML += `<li class="list-group-item">${game}</li>`;
+    });
+  }
+
+  // Event listener for removing games from the favourites list
   myGameList.addEventListener('click', (event) => {
-    let favGame = event.target.innerText;
+    if (event.target && event.target.nodeName === 'LI') {
+      let favGame = event.target.innerText;
+      removeFromFavouriteGames(favGame);
+    }
   });
+
+  // Remove a game from the favourites list and update the UI
+  function removeFromFavouriteGames(game) {
+    let index = myGames.indexOf(game);
+    if (index > -1) {
+      myGames.splice(index, 1);
+      renderFavouriteList();
+    }
+  }
 })();
